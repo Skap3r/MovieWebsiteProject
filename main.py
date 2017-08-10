ff21ef1a21cb6086ec150e75b266665a731a86ac
SkaperOmega <atif.slayer@gmail.com>
2017-08-10 23:14:57 +0530

    improved code readability following PEP 8 guidlines

--- main.py
+++ main.py
@@ -4,55 +4,57 @@ import fresh_tomatoes
 # Using Movie Class to create objects of my favorite movies.
 interstellar = movie.Movie("Interstellar",
                            "How to not grow old",
-                           "https://images-na.ssl-images-amazon.com/images/M/MV5BMjIxNTU4MzY4MF5BMl5BanBnXkFtZTgwMzM4ODI3MjE@._V1_QL50_SY1000_CR0,0,640,1000_AL_.jpg",
-                           "https://www.youtube.com/watch?v=zSWdZVtXT7E")
+                           "http://bit.ly/2uqGWDn",
+                           "https://youtu.be/zSWdZVtXT7E")
 
 the_martian = movie.Movie("The Martian",
                           "How to survive Mars",
-                          "https://images-na.ssl-images-amazon.com/images/M/MV5BMTc2MTQ3MDA1Nl5BMl5BanBnXkFtZTgwODA3OTI4NjE@._V1_QL50_SY1000_CR0,0,675,1000_AL_.jpg",
-                          "https://www.youtube.com/watch?v=ej3ioOneTy8")
+                          "http://bit.ly/2wvxJ9T",
+                          "https://youtu.be/ej3ioOneTy8")
 
 predestination = movie.Movie("Predestination",
                              "A man makes himself and on and on it goes",
-                             "https://images-na.ssl-images-amazon.com/images/M/MV5BMTAzODc3NjU1NzNeQTJeQWpwZ15BbWU4MDk5NTQ4NTMx._V1_QL50_SY1000_CR0,0,677,1000_AL_.jpg",
-                             "https://www.youtube.com/watch?v=-FcK_UiVV40")
+                             "http://bit.ly/2vQfSNi",
+                             "https://youtu.be/F_CCjXQ9aYw")
 
 prometheus = movie.Movie("Prometheus",
                          "In search of our ancestors",
-                         "https://images-na.ssl-images-amazon.com/images/M/MV5BMTY3NzIyNTA2NV5BMl5BanBnXkFtZTcwNzE2NjI4Nw@@._V1_QL50_SY1000_CR0,0,674,1000_AL_.jpg",
-                         "https://www.youtube.com/watch?v=34cEo0VhfGE")
+                         "http://bit.ly/2vQOsHN",
+                         "https://youtu.be/34cEo0VhfGE")
 
 the_ring = movie.Movie("The Ring",
                        "Don't watch TV",
-                       "https://images-na.ssl-images-amazon.com/images/M/MV5BNDA2NTg2NjE4Ml5BMl5BanBnXkFtZTYwMjYxMDg5._V1_QL50_.jpg",
-                       "https://www.youtube.com/watch?v=_PkgRhzq_BQ")
+                       "http://bit.ly/2hOzFHm",
+                       "https://youtu.be/_PkgRhzq_BQ")
 
 dead_silence = movie.Movie("Dead Silence",
-                           "Doll goes crazy", 
-                           "https://images-na.ssl-images-amazon.com/images/M/MV5BMjEyNDQwMTQ3OV5BMl5BanBnXkFtZTcwMTY5MzI0MQ@@._V1_QL50_.jpg",
-                           "https://www.youtube.com/watch?v=8b_HVtHmK30")
+                           "Doll goes crazy",
+                           "http://bit.ly/2vmJsJH",
+                           "https://youtu.be/8b_HVtHmK30")
 
-the_exorcism_of_emily_rose = movie.Movie("The Exorcism of Emily Rose",
-                           "A girl possessed by Satan gets exercised",
-                           "https://images-na.ssl-images-amazon.com/images/M/MV5BMTI2NTM5MjQ0Nl5BMl5BanBnXkFtZTcwNDAxNjAzMQ@@._V1_QL50_.jpg",
-                           "https://www.youtube.com/watch?v=jsGIpxlgENY")
+exorcism_of_emily_rose = movie.Movie("The Exorcism of Emily Rose",
+                                     "A possessed girl gets excercised",
+                                     "http://bit.ly/2vIL2pV",
+                                     "https://youtu.be/jsGIpxlgENY")
 
 chappie = movie.Movie("Chappie",
                       "A military robot that learns too much",
-                      "https://images-na.ssl-images-amazon.com/images/M/MV5BMTUyNTI4NTIwNl5BMl5BanBnXkFtZTgwMjQ4MTI0NDE@._V1_QL50_SY1000_CR0,0,674,1000_AL_.jpg",
-                      "https://www.youtube.com/watch?v=lyy7y0QOK-0")
+                      "http://bit.ly/2wLIQLk",
+                      "https://youtu.be/lyy7y0QOK-0")
 
 inception = movie.Movie("Inception",
                         "A story about a dream stealer",
-                        "https://images-na.ssl-images-amazon.com/images/M/MV5BMjAxMzY3NjcxNF5BMl5BanBnXkFtZTcwNTI5OTM0Mw@@._V1_QL50_SY1000_CR0,0,675,1000_AL_.jpg",
-                        "https://www.youtube.com/watch?v=YoHD9XEInc0")
+                        "http://bit.ly/2vSPJOq",
+                        "https://youtu.be/YoHD9XEInc0")
 
 the_matrix = movie.Movie("The Matrix",
                          "We are in a simulation!",
-                         "https://images-na.ssl-images-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg",
-                         "https://www.youtube.com/watch?v=m8e-FF8MsqU")
+                         "http://bit.ly/2wLFnw7",
+                         "https://youtu.be/m8e-FF8MsqU")
 
 # Adding movie objects to a list.
-movies = [interstellar, the_martian, the_ring, prometheus, predestination, dead_silence,
-           the_exorcism_of_emily_rose, the_matrix, chappie, inception]
-fresh_tomatoes.open_movies_page(movies) # This method takes in a list of Movie objects and creates a webpage with all the movies in it.
+movies = [interstellar, the_martian, the_ring, prometheus, predestination,
+          dead_silence, exorcism_of_emily_rose, the_matrix, chappie, inception]
+# The following method takes in a list of Movie objects and creates a
+# webpage with all the movies in it.
+fresh_tomatoes.open_movies_page(movies)
--- movie.py
+++ movie.py
@@ -1,18 +1,16 @@
 import webbrowser
 
+
 class Movie():
-        """ This class takes in movie information and stores it in it's instance variables."""
+    """Stores movie information in instance variables."""
 
-	def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
-		self.title = movie_title
-		self.storyline = movie_storyline
-		self.poster_image_url = poster_image
-		self.trailer_youtube_url = trailer_youtube
-		
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
+        self.title = movie_title
+        self.storyline = movie_storyline
+        self.poster_image_url = poster_image
+        self.trailer_youtube_url = trailer_youtube
+
+    def show_trailer(self):
         # opens the YouTube link provided and plays video
-        def show_trailer(self):
-                webbrowser.open(self.trailer_youtube)
-
-                                
-                
-	 	
+        webbrowser.open(self.trailer_youtube)
